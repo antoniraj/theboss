a8f72abcd9486490af0df590e548d0ca8de29441
antonirajsebastion@gmail.com <antonirajsebastion@gmail.com>
2016-11-18 08:43:50 +0530

    Quick Link

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -6,10 +6,13 @@
         </div>
         <div class="panel-body">
           <ul>
-            <li>Lorem ipsum dolor sit amet</li>
-            <li>Consectetur adipiscing elit</li>
-            <li>Integer molestie lorem at massa</li>
-            <li>Facilisis in pretium nisl aliquet</li>
+            <li><a href="https://staffingspecifix.tsheets.com/page/login" target="_blank">Enter Your Hours</a></li>
+           <li><a href="https://www.epaystubaccess.com/acctmgr.asp?pgid=browser&mdid=scr1&verid=eng" target="_blank">View Your Paystub(s)</a></li>
+          <li><a href="https://staffingspecifixinc.igloocommunities.com/events/employee_vacations" target="_blank">Request vacation</a></li>
+          <li><a href="#" target="_blank">Submit expenses</a></li>
+          <li><a href="#" target="_blank">Update your profile</a></li>
+          <li><a href="https://gordg.aviontego.com/RDWeb/Pages/en-US/login.aspx?ReturnUrl=default.aspx" target="_blank">Avionte</a></li>
+          
           </ul>
         </div>
       </div><!--panel-->
